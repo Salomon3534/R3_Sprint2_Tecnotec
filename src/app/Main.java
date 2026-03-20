93327bdef2784e059550517b43ea6c0486c1f163
Salomon <gojoxpolo@gmail.com>
2026-03-20 17:41:34 +0100

    Gestor invitados funcional
    1 de 4 VAMOOOS

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,21 +1,30 @@
 package app;
 
 import java.sql.SQLException;
-
+import db.DatabaseConnector;
 import util.TotalManagerEuskalEncounter;
 import view.ViewEuskalEncounter;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException  {
-	       
-        ViewEuskalEncounter vee = new ViewEuskalEncounter(new TotalManagerEuskalEncounter());
-        db.DatabaseConnector.conectar();
-     
-        vee.mainMenu();
-        
-        db.DatabaseConnector.cerrarConexion();
-          
-    }
+    public static void main(String[] args) {
+        try {
 
+            DatabaseConnector.getConexion(); 
+            
+
+            TotalManagerEuskalEncounter managerTotal = new TotalManagerEuskalEncounter();
+            
+
+            ViewEuskalEncounter view = new ViewEuskalEncounter(managerTotal);
+            view.mainMenu();
+            
+        } catch (SQLException e) {
+
+            System.err.println("error al iniciar la aplicacion: " + e.getMessage());
+        } catch (Exception e) {
+
+            System.err.println("error inesperado: " + e.getMessage());
+        }
+    }
 }
